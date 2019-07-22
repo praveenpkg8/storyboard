50896965d0d16d81290f37255b81b35021a67547
Your-Full-Name <praveenpkg8@gmail.com>
2019-07-22 22:36:00 +0530

    Updates

---
+++ precache-manifest.d7e5231c1993841cb305dae1516abd65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f587c3497b584c4b002fd96a291c172",
+    "url": "/storyboard/index.html"
+  },
+  {
+    "revision": "332b2446b2c0680e009e",
+    "url": "/storyboard/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4f342a20092e3f06a542",
+    "url": "/storyboard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "332b2446b2c0680e009e",
+    "url": "/storyboard/static/js/2.e15aca3c.chunk.js"
+  },
+  {
+    "revision": "4f342a20092e3f06a542",
+    "url": "/storyboard/static/js/main.4d2ac8b7.chunk.js"
+  },
+  {
+    "revision": "75fefb826723302001e7",
+    "url": "/storyboard/static/js/runtime~main.c8b569b9.js"
+  }
+]);
